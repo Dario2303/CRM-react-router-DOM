119228a5fce52eb58fb6b1526df697632658f716
dariorod <dario2303@gmail.com>
2023-04-19 12:24:31 -0300

    Form configure, use action function

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,9 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Form } from 'react-router-dom'
+import Formulario from '../components/Formulario'
+
+export function action () {
+  console.log('submit desde form...') 
+}
 
 function NuevoCliente() {
 
@@ -17,8 +22,18 @@ function NuevoCliente() {
         </button>
       </div>
 
-      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10'>
-        <p>Formulario aquí</p>
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+
+        <Form 
+          method='post'
+        >
+          <Formulario/>
+          <input 
+          type="submit"
+          className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+          value="Registrar cliente"/>
+        </Form>
+
       </div>
     </>
   )
